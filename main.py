3ff8404829f1ce424b5d5fb79eca1fc5200d4a2f
mazen <mazen.saleh.36@gmail.com>
2024-05-16 00:19:42 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk #pip install tkinter
+import time
+
+
+text_set = ["أحدٍ قفل دون هوجاسـه البـاب ... وأحدٍ غدت جدران بيتـه تفاكيـر","ياساكن الهوجاس ممسـاك ماطـاب ... راحـت مجاهيـم وجتنـا مغاتيـر"]
+
+sleep_time_between_lines = 1
+sleep_time_between_chars = 0.17
+
+def محمد_عبدة():
+    current_text = ""
+    for i, line in enumerate(text_set):
+        if i != 0:  
+            time.sleep(sleep_time_between_lines)
+        line += "\n"  
+        for char in line:
+            time.sleep(sleep_time_between_chars)  
+            current_text += char  
+            label.config(text=current_text)
+            root.update()
+
+
+root = tk.Tk()
+root.title("محمد عبده")
+root.geometry("500x500")  
+root.configure(bg='black')  
+label = tk.Label(root, fg="white", bg="black", font=("Helvetica", 16,"bold") )  
+label.pack()
+
+
+start_button = tk.Button(root, text="محمد عبده", command=محمد_عبدة, bg="lightgray" )
+start_button.place(x=225, y=100)  
+
+root.mainloop()
